ba98946ae2f7ee3c2c6340cab1bb2d178b9568e0
Djakaridja KEITA <djakaridja.keita@epsi.fr>
2021-03-25 10:18:59 +0100

    test fizz échoue

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -9,3 +9,8 @@ class FizzBuzzTest(unittest.TestCase):
     def test_nombre_est_negatif(self):
         with self.assertRaises(ArgurmentError):
             FizzBuzz.isFizzOrBuzz(-1)
+
+    def test_nombre_est_multiple_de_3_donne_fizz(self):
+        nb = 6
+        self.assertEqual(FizzBuzz.isFizzOrBuzz(nb), "Fizz")
+
